f9bce6552f14164c5e5c4b7712d09aa7cc10f3c6
erlhuang <erlhuang@ucsc.edu>
2019-08-26 11:19:52 -0700

    added directory naming according to manga being dl'd, finished with this? could clean up some of the code

--- Manga Downloader/mangaDownloader.py
+++ Manga Downloader/mangaDownloader.py
@@ -1,28 +1,32 @@
 import requests, os, bs4, re
 #TODO, make exceptions for weird manga with v in link
 #Give option if want in separate directories or same directory
-print('Hi, welcome to the Manga Downloader (title WIP obviously) Please provide a link from your preferred manga at Mangatown, starting with the lastchapter you would like to download.')
+print('Hi, welcome to the Manga Downloader. Please provide a link from your preferred manga at Mangatown, starting with the first chapter you would like to download.')
 while True:
     givenURL = input() #While look checks to make sure input is usable
     if 'mangatown.com' not in givenURL:
         print('You did not give a mangatown URL. Please try again.')
     else:
         break
-givenURL = 'http://www.mangatown.com/manga/yakusoku_no_neverland/c001/'
+#givenURL = 'http://www.mangatown.com/manga/yakusoku_no_neverland/c001/'
 res = requests.get(givenURL)
 res.raise_for_status() #Now check if mangatown link isn't a dead link
 
-print('Nice link bro')
-os.makedirs('promisedneverland', exist_ok=True)
+
 mangaSoup = bs4.BeautifulSoup(res.text, "html.parser")
 #Selects the img to download (img inside an anchor tag that's inside a viewier id)
+
 mangaImg = mangaSoup.select('#viewer a img')
-chapterNum = 1
-i = 1
 baseUrl = givenURL
 chapRegex = re.compile(r'c(\d+)')
-mo1 = chapRegex.search(baseUrl)
-numlength = len(str(mo1.group())) - 1
+nameRegex = re.compile(r'manga/(.*)/c')
+chapChars = chapRegex.search(baseUrl)
+mangaName = nameRegex.search(baseUrl)
+print('Series name: ' + str(mangaName.group(1)))
+os.makedirs(str(mangaName.group(1)), exist_ok=True)
+numlength = len(str(chapChars.group())) - 1
+chapterNum = 1
+i = 1
 while True:
     res = requests.get(givenURL)
     res.raise_for_status() #Now check if mangatown link isn't a dead link
@@ -30,13 +34,15 @@ while True:
     #Selects the img to download (img inside an anchor tag that's inside a viewier id)
     mangaImg = mangaSoup.select('#viewer a img')
     if mangaImg == []:
-        print('Could not find given image, ending')
+        print('Finished download')
         break
     else:
+        if(i == 1):
+            print('Beginning download of Chapter ' + str(chapterNum))
         imgUrl = mangaImg[0].get('src')
         res = requests.get(imgUrl)
         res.raise_for_status()
-        imageFile = open(os.path.join('promisedneverland', os.path.basename(str(chapterNum) + '-' + str(i)) + '.png'), 'wb')
+        imageFile = open(os.path.join(str(mangaName.group(1)), os.path.basename(str(chapterNum) + '-' + str(i)) + '.png'), 'wb')
         for chunk in res.iter_content(100000):
             imageFile.write(chunk)
         imageFile.close()
@@ -51,27 +57,22 @@ while True:
             strngChap = str(chapterNum)
             lenChapter = len(str(chapterNum))
             if(lenChapter == numlength): #Means they have same amt of digits
-                #directly replace w regex thingy
+                #directly replace w regex
                 givenURL = re.sub(r'c(\d+)', r'c%s' % str(strngChap), baseUrl)
             elif(lenChapter < numlength):
                 diffLen = numlength - lenChapter
                 if(diffLen == 1):
                     givenURL = re.sub(r'c(\d+)', r'c0%s' % str(strngChap), baseUrl)
-                    print('Going to:%s ' % givenURL )
                     baseUrl = givenURL
                 elif(diffLen == 2):
                     givenURL = re.sub(r'c(\d+)', r'c00%s' % str(strngChap), baseUrl)
-                    print('Going to:%s ' % givenURL )
+                    baseUrl = givenURL
+                elif(diffLen == 3):
+                    givenURL = re.sub(r'c(\d+)', r'c00%s' % str(strngChap), baseUrl)
                     baseUrl = givenURL
             else:
                 print("Didnt get diff numlength, break")
                 break
-            #option = mangaSoup.select('.chapter_select')
-            #print(str(option))
-            #chList = mangaSoup.select('option')
-            #givenURL = chList[chapterNum].get('value')
-            #print(givenURL)
-            #baseUrl = givenURL
             i = 1
         else:
             givenURL = baseUrl + newUrl
